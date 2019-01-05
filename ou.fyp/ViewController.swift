401af75274f5a1eb7fb0ecf137d6db65ca61af42
Chan Hong Wing <jason021075@me.com>
2019-01-05 14:13:35 +0800

    Initial Commit

---
+++ ou.fyp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ou.fyp
+//
+//  Created by Chan Hong Wing on 5/1/2019.
+//  Copyright © 2019 Chan Hong Wing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ou.fypTests/ou_fypTests.swift
@@ -0,0 +1,34 @@
+//
+//  ou_fypTests.swift
+//  ou.fypTests
+//
+//  Created by Chan Hong Wing on 5/1/2019.
+//  Copyright © 2019 Chan Hong Wing. All rights reserved.
+//
+
+import XCTest
+@testable import ou_fyp
+
+class ou_fypTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
